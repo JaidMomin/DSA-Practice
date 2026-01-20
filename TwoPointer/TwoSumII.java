c7140737c52b91dd7b038bd9829103e1297c3c6f
Mohamadjaid Momin <jaidmomin13@gmail.com>
2026-01-20 21:45:45 +0530

    Add files via upload

---
+++ TwoPointer/TwoSumII.java
@@ -0,0 +1,24 @@
+// Pattern : Two Pointer
+// Problem : Two Sum II - Input Array Is Sorted
+
+package TwoPointer;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while(i < j){
+            if (numbers[i] + numbers[j] > target){
+            j--;
+            }
+            else if (numbers[i] + numbers[j] < target){
+            i++;
+            }
+            else{
+                 return new int[]{i + 1, j + 1};
+            }
+        }
+        return new int[]{-1, -1};
+       
+    }
+}
